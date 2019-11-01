f52649f0293715c13986f4f05b8e37541edca9a8
Ross Hinman <rosshinman@Rosss-MacBook-Pro.local>
2019-11-01 10:32:40 -0400

    no updates, still working on weather

--- app.js
+++ app.js
@@ -20,23 +20,23 @@ app.use('/', function(req, res, next) {
     next();
 });
 
+function loadData(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if(this.readyState == 4 && this.status == 200){
+            var data = JSON.parse(this.responseText);
+            data.data.text.forEach(element => {
+                var node = document.createElement("li");
+                node.innerText = element;
+                document.getElementById("weather").appendChild(node)
+            });
+        }
+    };
+    xhttp.open("GET", "https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json", true);
+    xhttp.send();
+};
 
 app.get('/', function(req, res){
-    function loadData(){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function (){
-            if(this.readyState == 4 && this.status == 200){
-                var data = JSON.parse(this.responseText);
-                data.data.text.forEach(element => {
-                    var node = document.createElement("li");
-                    node.innerText = element;
-                    document.getElementById("weather").appendChild(node)
-                });
-            }
-        };
-        xhttp.open("GET", "https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json", true);
-        xhttp.send();
-    }
     res.render('index');
 });
 
